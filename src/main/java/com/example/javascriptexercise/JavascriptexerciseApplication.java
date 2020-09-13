5fc837a7f7c332fd76f2add84a3ee672c07a3b2c
Michael Fuglø <michael@fuglo.com>
2020-09-13 23:06:20 +0200

    Initial commit

---
+++ src/main/java/com/example/javascriptexercise/JavascriptexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.javascriptexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavascriptexerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavascriptexerciseApplication.class, args);
+    }
+
+}
